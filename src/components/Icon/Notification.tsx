0d74a98e63940ada0f01d0f711fd6b239b0d9cfb
Applewhz <76094766+Applewhz@users.noreply.github.com>
2022-02-15 18:27:55 +0800

    c1
    added in second option of notification menu with notification card

--- src/components/Icon/Notification.tsx
+++ src/components/Icon/Notification.tsx
@@ -3,11 +3,16 @@ import { IconContainer, IconButton } from '@epam/promo';
 import { ReactComponent as EmptyNotification } from '@epam/assets/icons/common/notification-warning-outline-18.svg';
 import { ReactComponent as Notification } from '@epam/assets/icons/common/notification-warning-fill-18.svg';
 import { Dropdown, DropdownBodyProps } from '@epam/uui-components';
-import { LinkButton, Button, Panel, Text, FlexRow } from "@epam/promo";
-import BasicNotificationCardExample from './NotificationCard';
+import { LinkButton, Button, Panel, Text, FlexRow, FlexCell, NotificationCard } from "@epam/promo";
 import { IDropdownToggler } from '@epam/uui';
+import css from './NotificationCard.module.scss';
 
 export default function BasicIconContainerExample() {
+
+    const actions = [
+        { name: 'Ok', action: () => {} },
+        { name: 'Cancel', action: () => {} },
+    ];
 
     const renderDropdownBody = (props: DropdownBodyProps) => {
         return (
@@ -17,7 +22,41 @@ export default function BasicIconContainerExample() {
                 <LinkButton caption='Click to close' onClick={ props.onClose } />
             </FlexRow>
         </Panel>
-    );
+        );
+    };
+
+    const renderDropdownBodyTest = (props: DropdownBodyProps) => {
+        return (
+            <Panel background='white' shadow={ true }>
+            <FlexCell cx={css.container}>
+        <NotificationCard
+          id={1}
+          key={'keyProps'}
+          color="gray60"
+          onClose={() => null}
+          onSuccess={() => null}
+        >
+          <Text size="36" font="sans" fontSize="14">
+            Common notification
+          </Text>
+        </NotificationCard>
+      </FlexCell>
+      <FlexCell width={350} cx={css.container}>
+        <NotificationCard
+          id={1}
+          key={'propsKey'}
+          actions={actions}
+          color="amber"
+          onClose={() => null}
+          onSuccess={() => null}
+        >
+          <Text size="36" font="sans" fontSize="14">
+            Warning notification with some buttons
+          </Text>
+        </NotificationCard>
+      </FlexCell>
+        </Panel>
+        );
     };
 
   return (
@@ -26,7 +65,11 @@ export default function BasicIconContainerExample() {
         renderBody={ renderDropdownBody }
         renderTarget={ (props: IDropdownToggler) => <IconButton icon={EmptyNotification} { ...props } /> }
         />
-        <BasicNotificationCardExample />
+        {/* <BasicNotificationCardExample /> */}
+        <Dropdown
+        renderBody={ renderDropdownBodyTest }
+        renderTarget={ (props: IDropdownToggler) => <IconButton icon={EmptyNotification} { ...props } /> }
+        />
     </>
   );
 }
