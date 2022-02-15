bdb045a4a66db2e988d33e9f03ed5a3f1f3237b7
Applewhz <76094766+Applewhz@users.noreply.github.com>
2022-02-15 18:00:27 +0800

    c1
    notification menu dropdown

--- src/components/Icon/Notification.tsx
+++ src/components/Icon/Notification.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import { IconContainer } from '@epam/promo';
+import React, { useState } from 'react';
+import { IconContainer, IconButton } from '@epam/promo';
 import { ReactComponent as EmptyNotification } from '@epam/assets/icons/common/notification-warning-outline-18.svg';
 import { ReactComponent as Notification } from '@epam/assets/icons/common/notification-warning-fill-18.svg';
+import { Dropdown, DropdownBodyProps } from '@epam/uui-components';
+import { LinkButton, Button, Panel, Text, FlexRow } from "@epam/promo";
 import BasicNotificationCardExample from './NotificationCard';
+import { IDropdownToggler } from '@epam/uui';
 
 export default function BasicIconContainerExample() {
-  const OnClickFunction = () => {};
+    
+    const [showNotification, setShowNotification] = useState(false);
+
+    const renderDropdownBody = (props: DropdownBodyProps) => {
+        return (
+            <Panel background='white' shadow={ true }>
+            <FlexRow padding='12' vPadding='12'>
+                <Text>Dropdown body content.</Text>
+                <LinkButton caption='Click to close' onClick={ props.onClose } />
+            </FlexRow>
+        </Panel>
+    );
+    };
 
   return (
     <>
-      <IconContainer icon={EmptyNotification} onClick={() => null} />
-      <IconContainer
-        icon={Notification}
-        color="blue"
-        flipY={true}
-        isDisabled={true}
-        onClick={() => null}
-      />
-      <IconContainer
-        icon={Notification}
-        color="violet"
-        style={{ transform: 'skew(-15deg, 18deg)' }}
-        onClick={() => null}
-      />
-      <BasicNotificationCardExample />
+        <Dropdown
+        renderBody={ renderDropdownBody }
+        renderTarget={ (props: IDropdownToggler) => <IconButton icon={EmptyNotification} { ...props } /> }
+        />
     </>
   );
 }
